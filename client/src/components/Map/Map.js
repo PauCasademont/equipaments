d71e3c7d6dfae425ade775891ba620fa5234c854
PauCasademont <u1953627@campus.udg.edu>
2021-02-12 22:21:30 +0100

    Created custom popup

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,8 +1,13 @@
 import { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Typography, IconButton } from '@material-ui/core';
 
 import './Map.css';
 import { getPublicFalcilities } from '../../actions/publicFacility';
+import consumption_icon from '../../images/consumption.png';
+import cost_icon from '../../images/cost.png';
+import indicators_icon from '../../images/indicators.png';
+
 
 const INITIAL_MAP_CONFIG = {center: [41.98311,2.82493], zoom: 14}
 
@@ -23,8 +28,24 @@ function Map() {
             />          
             {publicFacilities.map(({ _id, name, typology, coordinates, area }) => (
                 <Marker position={coordinates} key={_id}>
-                    <Popup>
-                        {name} <br /> Tipologia: {typology} <br /> {area ? `Superficie: ${area}` : ''}
+                    <Popup maxWidth='500'>
+                        <div className='popup-title-div'>
+                            <Typography className='popup-title-text' variant='h6' gutterBottom aling='center'>
+                                {name}
+                            </Typography>
+                        </div>
+                        <Typography className='popup-info' variant='body1' >
+                            Tipologia: {typology} <br /> {area ? <>{`Superficie: ${area} m`}<sup>2</sup></> : ''}
+                        </Typography>
+                        <IconButton className='icon-button'>
+                            <img className='icon' src={consumption_icon} alt='icon_btn' />
+                        </IconButton>
+                        <IconButton className='icon-button'>
+                            <img className='icon' src={cost_icon} alt='icon_btn' />
+                        </IconButton>
+                        {area && <IconButton className='icon-button'>
+                            <img className='icon' src={indicators_icon} alt='icon_btn' />
+                        </IconButton>}
                     </Popup>
                 </Marker>
             ))}
