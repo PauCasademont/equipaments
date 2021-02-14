f0c2a2ee59d4696440a70151b59caade41e03792
PauCasademont <u1953627@campus.udg.edu>
2021-02-14 15:50:55 +0100

    Refractor get icons

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,24 +1,39 @@
 import { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { Typography, IconButton } from '@material-ui/core';
+import L from 'leaflet';
 
 import './Map.css';
 import { getPublicFalcilities } from '../../actions/publicFacility';
-import consumption_icon from '../../images/consumption.png';
-import cost_icon from '../../images/cost.png';
-import indicators_icon from '../../images/indicators.png';
-
 
 const INITIAL_MAP_CONFIG = {center: [41.98311,2.82493], zoom: 14}
 
+function getIcons() {
+    const req = require.context('../../images', false, /.*\.png$/);
+    let res = {}
+    req.keys().map((key) => { 
+        res[key.replace('./', '').replace('.png', '')] = req(key).default; 
+    })   
+    return res;
+}
+
+function getIconMarker(icon) {
+    return new L.icon({
+        iconUrl: icon,
+        iconSize: [45, 45]
+    });
+}
+
 function Map() {
     const [publicFacilities, setPublicFacilities] = useState([]);
+    const icons = getIcons();
 
     useEffect(() => {
         getPublicFalcilities()
             .then((res) => {setPublicFacilities(res.data.result)})
             .catch((error) => {console.log(error)});
     }, []);
+
 
     return (                     
         <MapContainer center={INITIAL_MAP_CONFIG.center} zoom={INITIAL_MAP_CONFIG.zoom} scrollWheelZoom={true}>
@@ -27,7 +42,7 @@ function Map() {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />          
             {publicFacilities.map(({ _id, name, typology, coordinates, area }) => (
-                <Marker position={coordinates} key={_id}>
+                <Marker position={coordinates} key={_id} icon={getIconMarker(icons[`${typology}`])}>
                     <Popup maxWidth='500'>
                         <div className='popup-title-div'>
                             <Typography className='popup-title-text' variant='h6' gutterBottom aling='center'>
@@ -38,13 +53,13 @@ function Map() {
                             Tipologia: {typology} <br /> {area ? <>{`Superficie: ${area} m`}<sup>2</sup></> : ''}
                         </Typography>
                         <IconButton className='icon-button'>
-                            <img className='icon' src={consumption_icon} alt='icon_btn' />
+                            <img className='icon' src={icons.consum} alt='icon_btn' />
                         </IconButton>
                         <IconButton className='icon-button'>
-                            <img className='icon' src={cost_icon} alt='icon_btn' />
+                            <img className='icon' src={icons.cost} alt='icon_btn' />
                         </IconButton>
                         {area && <IconButton className='icon-button'>
-                            <img className='icon' src={indicators_icon} alt='icon_btn' />
+                            <img className='icon' src={icons.indicadors} alt='icon_btn' />
                         </IconButton>}
                     </Popup>
                 </Marker>
