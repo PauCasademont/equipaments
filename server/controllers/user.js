a91e238812414e33076ec5148dd1b90e34060b43
pau <61241126+PauCasademont@users.noreply.github.com>
2021-03-19 15:35:23 +0100

    Server user sign up

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,6 +2,26 @@ import bcrypt from 'bcryptjs';
 
 import UserModel from '../models/user.js';
 
+export const signup = async (req, res) => {
+    const { username, public_facility_id, password } = req.body;
+
+    try {
+        const oldUser = await UserModel.findOne({ username });
+        if (oldUser) res.status(400).json({ message: `User \'${username}\' already exist`});
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const result = await UserModel.create({
+            username,
+            public_facility_id,
+            password: hashedPassword
+        });
+        res.status(201).json({ result });        
+    } catch (error) {
+        res.status(500).json({ message: 'Could not sing up'});
+        console.log(error);
+    }
+}
+
 export const signin = async (req, res) => {
     const { username, password } = req.body;
 
